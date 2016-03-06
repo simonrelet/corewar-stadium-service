b3c4f11d0aff663a8cbfcd40bb181aae81f3b2b2
Simon Relet <simon.relet@gmail.com>
2016-03-06 15:17:43 +0100

    Added stadium service code

---
+++ app/corewar_stadium_service.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var app = require('express')();
+
+app.use('/', require('./stadium-router.js'));
+
+var server = app.listen(4201, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Corewar stadium service listening at http://%s:%s', host, port);
+});
